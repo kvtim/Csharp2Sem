8851d3615c10f611c9e1cf4b363a535309c40606
temakazik <61842660+temakazik@users.noreply.github.com>
2020-03-18 20:37:30 +0300

    Add files via upload
    3 LR

---
+++ Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public int PersonID { get; set; }
+
+        static private int id = 100;
+        public Person(int age, string name, string surname, int weight, int height)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Surname = surname;
+            this.Weight = weight;
+            this.Height = height;
+            Person.id += 1;
+            this.PersonID = Person.id;
+
+        }
+        public void SelectData(int b)
+        {
+            switch (b)
+            {
+                case 0:
+                    Console.WriteLine($" Age: {Age}");
+                    break;
+                case 1:
+                    Console.WriteLine($" Weight: {Weight}");
+                    break;
+                case 2:
+                    Console.WriteLine($" Height: {Height}");
+                    break;
+                case 3:
+                    Console.WriteLine($" Id: {PersonID}");
+                    break;
+            }
+
+        }
+
+        public void SelectData(string a)
+        {
+            switch (a)
+            {
+                case "Surname":
+                    Console.WriteLine($" Surname: {Surname}");
+                    break;
+                case "Name":
+                    Console.WriteLine($" Name: {Name}");
+                    break;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,60 +6,35 @@ namespace _1
     {
         static void Main(string[] args)
         {
-            int a;
-            string s1 = DateTime.Now.ToString();
-            string s2 = DateTime.Now.ToString("F");
-          
-            Console.WriteLine(s2);
-            Console.WriteLine(s1);
-            Console.WriteLine(" In first date: ");
-            m(s2);
-            Console.WriteLine(" In second date: ");
-            m(s1);
-           
-        }
-        static void m(string s1)
-        {
-            int n0 = 0, n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0, n8 = 0, n9 = 0;
-            for (int i = 0; i < s1.Length; i++)
+            while (true)
             {
-                switch (s1[i])
-                {
-                    case '0':
-                        n0++;
-                        break;
-                    case '1':
-                        n1++;
-                        break;
-                    case '2':
-                        n2++;
-                        break;
-                    case '3':
-                        n3++;
-                        break;
-                    case '4':
-                        n4++;
-                        break;
-                    case '5':
-                        n5++;
-                        break;
-                    case '6':
-                        n6++;
-                        break;
-                    case '7':
-                        n7++;
-                        break;
-                    case '8':
-                        n8++;
-                        break;
-                    case '9':
-                        n9++;
-                        break;
-                    default: break;
-                }
+                Person person1 = new Person(18, "Egor", "Shirko", 49, 205);
+                Console.Write(" Select the data of 1 person: Name or Surname: ");
+                string StringData1 = Console.ReadLine();
+                person1.SelectData(StringData1);
+                Console.WriteLine(" Select the data of 1 person: \n 0: Age; \n 1: Weight; \n 2: Height; \n 3: Id; ");
+                int IntData1 = Convert.ToInt32(Console.ReadLine());
+                person1.SelectData(IntData1);
+                Console.WriteLine(" Go to next person Yes or No: ");
+                string b = Console.ReadLine();
+                if (b == "Yes")
+                    break;
             }
-            Console.WriteLine($" Zeros: {n0}\n Ones: {n1}\n Twos: {n2}\n Triples: {n3}\n Foures: {n4}\n Fives: {n5}\n" +
-           $" Sixes: {n6}\n Sevens: {n7}\n Eights: {n8}\n Nines: {n9}");
+            while (true)
+            {
+                Person person2 = new Person(20, "Kirill", "Rogov", 33, 170);
+                Console.Write(" Select the data of 2 person: Name or Surname: ");
+                string StringData2 = Console.ReadLine();
+                person2.SelectData(StringData2);
+                Console.WriteLine(" Select the data of 2 person: \n 0: Age; \n 1: Weight; \n 2: Height; \n 3: Id; ");
+                int IntData2 = Convert.ToInt32(Console.ReadLine());
+                person2.SelectData(IntData2);
+                Console.WriteLine(" Quit the program? Yes or No: ");
+                string d = Console.ReadLine();
+                if (d == "Yes")
+                    break;
+            }
+
         }
     }
 }
