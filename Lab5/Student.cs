5c96ef0a837f83ddbbbb23da599aab82760b14a6
Artem <61842660+temakazik@users.noreply.github.com>
2020-04-30 02:39:24 +0300

    Update Student.cs

--- Lab5/Student.cs
+++ Lab5/Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,13 +15,66 @@ namespace LR_5
             this.Course = course;
             this.Faculty = faculty;
         }
-        public static int Sort(double Student1, double Student2)
+        public static void Sort(ref StudentSecialty[] student, int parameter)
         {
-            if (Student1 > Student2)
-                return 1;
-            else
-                return 2;
+            for (int i = 0; i < student.Length; i++)
+            {
+                for (int j = 0; j < student.Length - 1; j++)
+                {
+                    switch (parameter)
+                    {
+                        case 1:
+                            if (student[j].Age > student[j + 1].Age)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                        case 2:
+                            if (student[j].Weight > student[j + 1].Weight)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                        case 3:
+                            if (student[j].Height > student[j + 1].Height)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                        case 4:
+                            if (student[j].Course > student[j + 1].Course)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                        case 5:
+                            if (student[j].SkippingHours > student[j + 1].SkippingHours)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                        case 6:
+                            if (student[j].AvarageMark > student[j + 1].AvarageMark)
+                            {
+                                StudentSecialty temp = student[j];
+                                student[j] = student[j + 1];
+                                student[j + 1] = temp;
+                            }
+                            break;
+                    }
 
+                }
+            }
         }
     }
 }
